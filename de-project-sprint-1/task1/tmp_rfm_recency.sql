82628a49f7a4d744f8e25d0978d542ec1ebf56c3
malkina_alice <73836369+MalkinaAlice13@users.noreply.github.com>
2022-12-29 13:10:42 +0300

    Update tmp_rfm_recency.sql
    NTILE - автоматически распределяет строки упорядоченной секции в заданное количество групп

--- de-project-sprint-1/task1/tmp_rfm_recency.sql
+++ de-project-sprint-1/task1/tmp_rfm_recency.sql
@@ -1,31 +1,18 @@
 insert into analysis.tmp_rfm_recency (user_id, recency)
 with 
 users as (
-	select distinct user_id from analysis.view_orders),
+	select distinct user_id from analysis.view_orders ),
 users_and_order_ts as (
 	select  
 		user_id,
 		max(order_ts) order_ts
 	FROM analysis.view_orders
 	where status = 4
-	group by 1),
-num_order_ts as (
-	select 
-		u.user_id, 
-		u_t.order_ts,
-		row_number () over (order by u_t.order_ts) num
-	from users u 
-	left join users_and_order_ts u_t 
-	on u.user_id = u_t.user_id),
-itog as (
-	select *,
-	case 
-		when order_ts is null then 1
-		when num between 1 and 188 then 1
-		when num between 189 and 388 then 2
-		when num between 389 and 588 then 3
-		when num between 589 and 788 then 4
-		else '5' end recency 
-	from num_order_ts)
+	group by 1)
 
-select user_id, recency from itog
+select 
+	u.user_id, 
+	NTILE(5) OVER(ORDER by coalesce (u_t.order_ts, (select min(order_ts) from users_and_order_ts))) recency
+from users u 
+left join users_and_order_ts u_t 
+on u.user_id = u_t.user_id
