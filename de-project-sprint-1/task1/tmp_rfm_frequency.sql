3fea0d38d08370467be37fe1f30477a27b974798
malkina_alice <73836369+MalkinaAlice13@users.noreply.github.com>
2022-12-29 12:21:02 +0300

    Update tmp_rfm_frequency.sql
    NTILE - автоматически распределяет строки упорядоченной секции в заданное количество групп

--- de-project-sprint-1/task1/tmp_rfm_frequency.sql
+++ de-project-sprint-1/task1/tmp_rfm_frequency.sql
@@ -1,30 +1,17 @@
 insert into analysis.tmp_rfm_frequency (user_id, frequency)
 with 
 users as (
-	select distinct user_id from analysis.view_orders),
-users_and_order_ts as (
+	select distinct user_id from production.orders),
+count_status as (
 	select  
 		user_id,
 		count(status) count_orders 
-	FROM analysis.view_orders
+	FROM production.orders
 	where status = 4
-	group by 1),
-over_count_ord as (
-	select 
-		u.user_id,
-		count_orders,
-		row_number () over(order by count_orders) over_count_orders
-	from users u left join users_and_order_ts u_t
-	on u.user_id = u_t.user_id),
-itog as (
-	select *,
-	case 
-		when count_orders is null then 1
-		when over_count_orders between 1 and 188 then 1
-		when over_count_orders between 189 and 388 then 2
-		when over_count_orders between 389 and 588 then 3
-		when over_count_orders between 589 and 788 then 4
-		else 5 end frequency 
-	from over_count_ord)
+	group by 1)
 
-select user_id, frequency from itog
+select 
+	u.user_id,
+	NTILE(5) OVER(ORDER by coalesce(u_t.count_orders, 0)) frequency
+from users u left join count_status u_t
+on u.user_id = u_t.user_id
