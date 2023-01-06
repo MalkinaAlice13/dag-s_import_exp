357021156f9c8a19f89ca72ceb3bd9281bd3528b
malkina_alice <73836369+MalkinaAlice13@users.noreply.github.com>
2023-01-06 16:03:32 +0300

    Update orders_view.sql
    переписала решение задачи с использованием оконной функции - проранжировать изменения статусов каждого заказа в обратном порядке и выбрать с 1-м номером; также дополнила файл еще одним вариантом реализации решения задачи - "форточки в оконных функциях"

--- de-project-sprint-1/task2/orders_view.sql
+++ de-project-sprint-1/task2/orders_view.sql
@@ -1,31 +1,38 @@
 CREATE OR REPLACE VIEW analysis.view_orders AS 
 
 with 
-order_dttm as (
-    select
-        order_id ,
-        max(dttm) dttm 
-    from production.orderstatuslog 
-    group by 1),
-itog as (
-        select od.*, 
-        vo.status_id as status
-    from order_dttm od
-    left join production.orderstatuslog vo 
-    on od.order_id = vo.order_id and od.dttm = vo.dttm),
-old_orders as (
-    select 
-        order_id,
-        order_ts,
-        user_id,
-        bonus_payment,
-        payment,
-        "cost",
-        bonus_grant
-    from production.orders)
+status as (
+	select order_id, status_id from (
+select *,
+row_number () over (partition by order_id order by dttm desc) over_
+from production.orderstatuslog
+	) tabl_over where over_ = 1)
 
-select 
-    o.*,
-    i.status
-from old_orders o 
-join itog i on o.order_id = i.order_id
+select DISTINCT
+	o.order_id,
+	o.order_ts,
+	o.user_id,
+	o.bonus_payment,
+	o.payment,
+	o."cost",
+	o.bonus_grant,
+	s.status_id as status 
+from production.orders o
+join status s on o.order_id = s.order_id
+
+----НО МОЖНО И ТАК----
+
+SELECT 
+    DISTINCT o.order_id,
+    o.order_ts,
+    o.user_id,
+    o.bonus_payment,
+    o.payment,
+    o."cost",
+    o.bonus_grant,
+    LAST_VALUE(p.status_id) OVER (PARTITION BY p.order_id ORDER BY dttm ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS status
+FROM 
+    production.orders AS o
+JOIN
+    production.orderstatuslog AS p
+        USING(order_id) 
